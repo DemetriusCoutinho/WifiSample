265f4b5f44e10e45079e77681ded4a241428e94b
Demetrius Coutinho <wdcrr@hotmail.com>
2019-10-29 23:45:43 -0300

    Wifi P2P

--- app/src/main/java/com/example/wifisample/MainActivity.java
+++ app/src/main/java/com/example/wifisample/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.wifisample;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wifisample.broadcast.WifiP2PBroadCast;
 
 import java.util.List;
 
@@ -22,43 +24,67 @@ public class MainActivity extends AppCompatActivity {
     private WifisRecyclerAdapter mWifisRecyclerAdapter;
     private List<ScanResult> mListScanResults;
 
+    // WifiP2p
+    private WifiP2pManager mWifiP2pManager;
+    private WifiP2pManager.Channel mChannel;
+    private BroadcastReceiver mBroadcastReceiver;
+    private IntentFilter mIntentFilter;
+    private WifiP2pManager.PeerListListener mPeerListListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.mContext = this;
-        this.mViewHolder = new ViewHolder();
-        mWifiManager = (WifiManager) this.mContext.getSystemService(Context.WIFI_SERVICE);
-
-        this.mViewHolder.mRecyclerView = this.findViewById(R.id.recycler_wifis);
-
-        this.mViewHolder.mRecyclerView.setHasFixedSize(true);
-
-        this.mViewHolder.mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        BroadcastReceiver wifiScanReceptor = new BroadcastReceiver() {
+//        this.mViewHolder = new ViewHolder();
+//        mWifiManager = (WifiManager) this.mContext.getSystemService(Context.WIFI_SERVICE);
+//        this.mViewHolder.mRecyclerView = this.findViewById(R.id.recycler_wifis);
+//        this.mViewHolder.mRecyclerView.setHasFixedSize(true);
+//        this.mViewHolder.mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        BroadcastReceiver wifiScanReceptor = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//                boolean sucesso = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+//                if (sucesso) {
+//                    scanSucesso();
+//                } else {
+//                    scanFalha();
+//                }
+//            }
+//        };
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+//        this.mContext.registerReceiver(wifiScanReceptor, intentFilter);
+//        boolean sucesso = mWifiManager.startScan();
+//        if (!sucesso) {
+//            this.scanFalha();
+//        }
+        this.mPeerListListener = new WifiP2pManager.PeerListListener() {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                boolean sucesso = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
-
-                if (sucesso) {
-                    scanSucesso();
-                } else {
-                    scanFalha();
-                }
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
 
             }
         };
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        this.mContext.registerReceiver(wifiScanReceptor, intentFilter);
+        this.mIntentFilter = new IntentFilter();
+        this.mWifiP2pManager = (WifiP2pManager) this.mContext.getSystemService(Context.WIFI_P2P_SERVICE);
+        this.mChannel = this.mWifiP2pManager.initialize(this.mContext, getMainLooper(), null);
+        this.mWifiP2pManager.discoverPeers(this.mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(mContext, "Sucesso ! ", Toast.LENGTH_SHORT).show();
+            }
 
-        boolean sucesso = mWifiManager.startScan();
+            @Override
+            public void onFailure(int i) {
 
-        if (!sucesso) {
-            this.scanFalha();
-        }
+            }
+        });
+        this.mBroadcastReceiver = new WifiP2PBroadCast(this.mWifiP2pManager, this.mChannel, this, this.mPeerListListener);
+        this.mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        this.mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        this.mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        this.mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
     }
 
@@ -67,6 +93,18 @@ public class MainActivity extends AppCompatActivity {
         this.mWifisRecyclerAdapter = new WifisRecyclerAdapter(this.mListScanResults, this.mContext);
         this.mViewHolder.mRecyclerView.setAdapter(this.mWifisRecyclerAdapter);
         this.mViewHolder.mRecyclerView.getAdapter().notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(this.mBroadcastReceiver, this.mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(this.mBroadcastReceiver);
     }
 
     private void scanFalha() {
---
+++ app/src/main/java/com/example/wifisample/broadcast/WifiP2PBroadCast.java
@@ -0,0 +1,47 @@
+package com.example.wifisample.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.wifisample.MainActivity;
+
+public class WifiP2PBroadCast extends BroadcastReceiver {
+    private WifiP2pManager mWifiP2pManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mMainActivity;
+    private WifiP2pManager.PeerListListener mPeerListListener;
+
+    public WifiP2PBroadCast(WifiP2pManager mWifiP2pManager, WifiP2pManager.Channel mChannel, MainActivity mMainActivity, WifiP2pManager.PeerListListener peerListListener) {
+        this.mWifiP2pManager = mWifiP2pManager;
+        this.mChannel = mChannel;
+        this.mMainActivity = mMainActivity;
+        this.mPeerListListener = peerListListener;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+//                Toast.makeText(context, "Wifi P2P Ativo ! ", Toast.LENGTH_SHORT).show();
+                Log.i("BroadCastWifiP2p", "Wifi P2p Ativo !");
+            } else {
+                Toast.makeText(context, "Wifi P2P Desativado ! ", Toast.LENGTH_SHORT).show();
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            if (this.mWifiP2pManager != null) {
+                this.mWifiP2pManager.requestPeers(this.mChannel, this.mPeerListListener);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+
+        }
+    }
+}
